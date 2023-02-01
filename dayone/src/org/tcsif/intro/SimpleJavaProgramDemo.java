7a3c4ca50e9a8d043e8351c85c53e89629df301f
ganesh1151 <ganeshchavan4561@gmail.com>
2023-02-01 23:03:07 +0530

    First Commit

---
+++ dayone/src/org/tcsif/intro/SimpleJavaProgramDemo.java
@@ -0,0 +1,10 @@
+package org.tcsif.intro;
+
+public class SimpleJavaProgramDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java Full Stack Development");
+	}
+
+}
